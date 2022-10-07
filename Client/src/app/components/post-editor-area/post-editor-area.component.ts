12af49c6d219b6dabfff5b4180d40be23c3ef424
athenexfolk <folkadpocket99@gmail.com>
2022-10-08 02:28:26 +0700

    notification

--- Client/src/app/components/post-editor-area/post-editor-area.component.ts
+++ Client/src/app/components/post-editor-area/post-editor-area.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-post-editor-area',
   templateUrl: './post-editor-area.component.html',
@@ -7,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostEditorAreaComponent implements OnInit {
 
+
+  
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  
 
 }
