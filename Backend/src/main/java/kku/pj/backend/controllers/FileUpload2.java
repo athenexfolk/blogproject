28523b3144c9528e2baf6fbb84ba06f510e6de4e
VillSource <anirut1800@gmail.com>
2022-10-11 23:44:54 +0700

    Add file uploade

---
+++ Backend/src/main/java/kku/pj/backend/controllers/FileUpload2.java
@@ -0,0 +1,65 @@
+package kku.pj.backend.controllers;
+
+import kku.pj.backend.dto.ImageEntityDto;
+import kku.pj.backend.entities.ImageEntity;
+import kku.pj.backend.entities.V1.Image;
+import kku.pj.backend.services.IImageService;
+import kku.pj.backend.utills.Author;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v2")
+@CrossOrigin("*")
+public class FileUpload2 {
+    private final IImageService imageService;
+    private final Author author;
+
+    public FileUpload2(IImageService imageService, Author author) {
+        this.imageService = imageService;
+        this.author = author;
+    }
+
+
+    @PostMapping(value = "/upload/image" )
+    public ResponseEntity<ImageEntityDto> uplaodImage(
+            @RequestParam("image") Optional<MultipartFile> file,
+            @RequestParam Optional<String> atl,
+            @RequestParam Optional<String> name,
+            @CurrentSecurityContext SecurityContext securityContext
+    ) {
+        String type = file.get().getContentType();
+        String username = author.getUsernameFromContext(securityContext);
+
+        if(file.isEmpty() || !type.contains("image/"))
+            return  new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+
+        if(username.equals(author.ANONYMOUS_USER))
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+
+        try {
+            var imgPath = imageService.saveImgToFileSystem(file.get().getBytes(),type);
+            ImageEntity img = new ImageEntity();
+            img.setUsername(username);
+            img.setAlt(atl.orElse(""));
+            img.setName(name.orElse(""));
+            img.setUrl(imgPath);
+
+            img = imageService.add(img);
+            return ResponseEntity.ok(new ImageEntityDto(img));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+}
